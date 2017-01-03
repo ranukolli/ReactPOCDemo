b0d919e87fea89ac3efd8ff5f9ba70e9709ff84e
ranukolli <kolliranu@yahoo.com>
2017-01-03 11:59:32 -0700

    ReactPOC

---
+++ PeopleSearchApplication-develop/Search/DAL/SearchData.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Search;
+
+namespace PeopleSearch.DAL
+{
+
+    public interface IsearchData
+    {
+       List<SearchModel> GetPersonDetails(string filter);
+    }
+    public class SearchData : IsearchData    
+    {
+    private PeopleSearchEntities _db;
+    public SearchData()
+    {
+
+    }
+    public SearchData(PeopleSearchEntities db)
+    {
+        _db = db;
+    }
+
+
+    public List<SearchModel> PersonList()
+    {
+            List<SearchModel> people = new List<SearchModel>();
+            people.Add(
+                        new SearchModel
+                        {
+                            FirstName = "Danny",
+                            LastName = "Aguilar",
+                            Team = "Justice League/Xmen",
+                            Avatar = "Batman"
+                        }
+                      );
+            people.Add(
+                        new SearchModel
+                        {
+                            FirstName = "Ranadheer",
+                            LastName = "Kolli",
+                            Team = "Justice League",
+                            Avatar = "Superman"
+                        }
+                       );
+            people.Add(
+                    new SearchModel
+                    {
+                        FirstName = "Ray",
+                        LastName = "Loveless",
+                        Team = "Justice League",
+                        Avatar = "Green Arrow"
+                    }
+                );
+
+            people.Add(
+                  new SearchModel
+                  {
+                      FirstName = "Jeff",
+                      LastName = "Arrowsmith",
+                      Team = "Justice League",
+                      Avatar = "XXX"
+                  }
+              );
+            people.Add(
+                  new SearchModel
+                  {
+                      FirstName = "Michael",
+                      LastName = "Rentmeister",
+                      Team = "Justice League",
+                      Avatar = "XXX"
+                  }
+              );
+
+            return people;
+
+    }
+    public List<SearchModel> GetPersonDetails(string filter)
+        {
+            List<SearchModel> details = new List<SearchModel>();
+
+            if (_db == null)
+            {
+                _db = new PeopleSearchEntities();
+            }
+
+            //var details = (from pd in _db.PersonDetails
+            //               join p in _db.Pictures on pd.PersonId equals p.PersonId
+            //               where (pd.FirstName.Contains(filter) || pd.LastName.Contains(filter))
+            //               select new SearchModel
+            //               {
+            //                   PersonId = pd.PersonId,
+            //                   FirstName = pd.FirstName,
+            //                   LastName = pd.LastName,
+            //                   Age = pd.Age,
+            //                   Address = pd.Address,
+            //                   Interests = pd.Interests,
+            //                   Image = p.Picture1
+            //               }).ToList();
+
+            List<SearchModel> people = PersonList();
+
+            foreach(var item in people)
+            {
+                if(item.FirstName.ToLower().Contains(filter) || item.LastName.ToLower().Contains(filter))
+                {
+                    details.Add(item);
+                }
+                  
+            }
+
+            return details;
+
+        }
+    }
+}
---
+++ PeopleSearchApplication-develop/Search/Scripts/Components/Search/Search_View.jsx
@@ -0,0 +1,90 @@
+﻿// ===============================================
+// <Search />
+// ===============================================
+
+Search.View = React.createClass({
+    // ----------------------------
+    render: function () {
+        var resx = this.props.Resources;
+        var orgGrid = this.renderGrid();
+
+        return (
+            <div id='org-search'>
+            	<h1 className='main-statement'>People Search</h1>
+            	<p className='desc-statement'>Start Typing FirstName Or LastName Here To Search People</p>
+
+            	<div className='separator x-small' />
+
+            	<IconInput.View type='search' onInput={this.inputHandler} placeholer='' />
+
+                {orgGrid}
+            </div>
+        );
+    },
+
+    // ---------------------------------
+    getInitialState: function(){
+        return {
+            orgs: []
+        };
+    },
+
+    // ----------------------------
+    renderGrid: function(){
+        if (!this.state.filter || !this.state.searchPerformed) return;
+
+        // var resx = this.props.Resources;
+        var gridProps = {
+            type: 'static',
+            name: 'organizations',
+            headers: [
+                'First Name',
+                'Last Name',
+                'Team',
+                'Avatar'
+            ],
+            headers_map: {
+                'FirstName':'First Name',
+                'LastName': 'Last Name',
+                'Team':'Team',
+                'Avatar':'Avatar'
+            },
+            zebraRows: true,
+            data: this.state.orgs
+        };
+
+        return (
+        	<div className='org-search__results'>
+        		<div className='separator small' />
+	        	<h2>People Details</h2>
+	        	<p>Search results for given values</p>
+
+	        	<div className='separator small' />
+
+	        	<Grid.View {...gridProps} />
+        	</div>
+    	);
+    },
+
+    // ----------------------------
+    inputHandler: function (event) {
+        var filter = event.target.value;
+        this.setState({ filter });
+        this.fetchResults(filter);
+    },
+
+    // debounce method will wait for complete user input.
+    fetchResults: searchStore.debounce(function(filter){
+        searchStore.getFilteredGroups(filter).success(orgs => {
+            // Add custom cells to data model which is mapped
+            // to the grid component (Name/Mnemonic as an anchor).
+            orgs = orgs.map(o => {
+                var {...org} = o;
+                return o;
+            });
+            // Update the state and render the new grid of data.
+            this.setState({ orgs, searchPerformed: true });
+        })
+    },300),
+
+});
